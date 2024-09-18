70d03185ef3df9d3d8d8171e78075fa64f8c1e76
THIERRY DONALD NDOUNKEU <77206591+ThierryNdounkeu@users.noreply.github.com>
2024-09-18 07:56:52 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
   profile = "default"
 }
 
-resource "aws_instance" "myJPMORGAN" {
+resource "aws_instance" "PMORGAN" {
   ami           = "ami-029kjfjjgjjhffff"
   instance_type = "t2.micro"
 
